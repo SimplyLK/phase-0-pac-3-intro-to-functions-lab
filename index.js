7df1af8e5eed6a1bfa1e12d9be5f8b896475a977
Laura <laura.kan@hotmail.com>
2021-03-18 12:22:49 +1000

    Done with assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+return string.toUpperCase() }
+
+function whisper(string) {
+return string.toLowerCase() }
+
+function logShout(string) {
+console.log (string.toUpperCase()) }
+
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma (string) {
+  var canthear = "I can't hear you!";
+  var yesindeed = "YES INDEED!";
+  var loveyou = "I love you, too.";
+ 
+  if (string.toLowerCase(string) === string) {
+    return canthear;
+  }
+  else if (string.toUpperCase(string) === string) {
+    return yesindeed;
+  }
+  else if ("I love you, Grandma." === string) {
+    return loveyou}
+  }
